3d22d0eb5c93a918d569ddebdf50ed5c1d3a738e
nguyenquan-2510 <quannguyenminhbl@gmail.com>
2025-10-29 21:52:16 +0700

    Yayy added snake object :)

--- conf.lua
+++ conf.lua
@@ -1,18 +1,26 @@
 local love = require 'love'
 
 -- Variables
-SIU = "Hello, Snake Game!"
 GREEN = {173 / 255, 204 / 255, 96 / 255, 255 / 255}
 DARK_GREEN = {101 / 255, 140 / 255, 43 / 255, 255 / 255}
 CELL_SIZE = 30
 CELL_COUNT = 25
 OFFSET = 75
-SCREEN_W = CELL_SIZE * CELL_COUNT + OFFSET
-SCREEN_H = CELL_SIZE * CELL_COUNT + OFFSET
+SCREEN_W = CELL_SIZE * CELL_COUNT + OFFSET * 2
+SCREEN_H = CELL_SIZE * CELL_COUNT + OFFSET * 2
 ------------
+-- My Custom Functions Here !
+
+-- Add 2 Vector2
+function Vector2Add(Vector2_A, Vector2_B)
+    if #Vector2_A ~= 2 or #Vector2_B ~= 2 then
+        error("Both parameters must be Vector2")
+    end
+    return { Vector2_A[1] + Vector2_B[1], Vector2_A[2] + Vector2_B[2] }
+end
 
 function love.conf(t)
-    t.console = false
+    t.console = true
     t.window.width = SCREEN_W
     t.window.height = SCREEN_H
     t.window.title = "Snake Game - Lua x LOVE2D"
--- main.lua
+++ main.lua
@@ -1,16 +1,59 @@
 
 local love = require 'love'
 
+local snake = {}
+
+snake.body = { {6, 9}, {5, 9}, {4, 9} }
+snake.direction = {1, 0}
+snake.addSegment = false
+
+function snake:draw()
+    for i = 1, #self.body do
+        local x = self.body[i][1]
+        local y = self.body[i][2]
+        love.graphics.setColor(DARK_GREEN)
+        love.graphics.rectangle("fill", OFFSET + x * CELL_SIZE, OFFSET + y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+    end
+end
+
+function snake:update()
+    -- Ehh nothing yet
+end
+
+function snake:move(key)
+    if key == "up" and self.direction[2] ~= 1 then
+        self.direction = {0, -1}
+        print(self.direction[1], self.direction[2])
+    elseif key == "down" and self.direction[2] ~= -1 then
+        self.direction = {0, 1}
+        print(self.direction[1], self.direction[2])
+    elseif key == "left" and self.direction[1] ~= 1 then
+        self.direction = {-1, 0}
+        print(self.direction[1], self.direction[2])
+    elseif key == "right" and self.direction[1] ~= -1 then
+        self.direction = {1, 0}
+        print(self.direction[1], self.direction[2])
+    end
+end
+
+function love.keypressed(key)
+    snake:move(key)
+end
+
 function love.load()
-    eat = love.audio.newSource("assets/eat.mp3", "static")
-    wall = love.audio.newSource("assets/wall.mp3", "static")
+    local eat = love.audio.newSource("assets/eat.mp3", "static")
+    local wall = love.audio.newSource("assets/wall.mp3", "static")
 end
 
 function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.setBackgroundColor(GREEN)
-end
 
-print(SIU)
+    -- Draw background and border
+    love.graphics.setBackgroundColor(0, 0, 0)
+    love.graphics.setColor(DARK_GREEN)
+    love.graphics.rectangle("line", OFFSET - 5, OFFSET - 5, SCREEN_W - 2 * OFFSET + 10, SCREEN_H - 2 * OFFSET + 10)
+
+    snake:draw()
+end
