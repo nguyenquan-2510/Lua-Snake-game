8e98fa375b8972400cbac78889cef57d76e040bc
nguyenquan-2510 <quannguyenminhbl@gmail.com>
2025-10-30 12:01:48 +0700

    Added function to check if 2 vectors are equal and find if a vector is contained in a table !

--- conf.lua
+++ conf.lua
@@ -21,6 +21,24 @@ function Vector2Add(Vector2_A, Vector2_B)
     return { Vector2_A[1] + Vector2_B[1], Vector2_A[2] + Vector2_B[2] }
 end
 
+-- Compare 2 vectors
+function Vector2Equal(Vector2_A, Vector2_B)
+    if #Vector2_A ~= 2 or #Vector2_B ~= 2 then
+        error("Both parameters must be Vector2")
+    end
+    return Vector2_A[1] == Vector2_B[1] and Vector2_A[2] == Vector2_B[2]
+end
+
+-- Element in table
+function ElementInTable(Vector2_Element, Vector2_Table)
+    for i = 1, #Vector2_Table do
+        if Vector2Equal(Vector2_Element, Vector2_Table[i]) then
+            return true
+        end
+    end
+    return false
+end
+
 -- Check interval function
 function CheckInterval(interval)
     local CurentTime = love.timer.getTime()
